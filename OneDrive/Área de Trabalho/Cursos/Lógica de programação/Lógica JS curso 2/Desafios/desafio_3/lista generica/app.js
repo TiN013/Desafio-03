ca81c494a66510463796f13924b2f98a750f627f
Denis Alves <denismonteiroalves@gmail.com>
2025-02-20 17:50:15 -0300

    Desafio-03

---
+++ OneDrive/Área de Trabalho/Cursos/Lógica de programação/Lógica JS curso 2/Desafios/desafio_3/lista generica/app.js
@@ -0,0 +1,17 @@
+//Primeiro desafio: crie uma lista de linguagens de programação e adicione itens.
+let LinguagensDeProgramacao = ['Python','JavaScript','Kotlin','C','C++'];
+LinguagensDeProgramacao.push('Java','Ruby','GoLang');
+console.log(LinguagensDeProgramacao);
+
+//Crie uma lista com nomes e mostre apenas o primeiro elemento.
+let nomes = ['Denis','Bianca','Fawkes'];
+console.log(nomes[0]);
+
+
+//Crie uma lista com nomes e mostre apenas o segundo elemento.
+let nomess = ['Denis','Bianca','Fawkes'];
+console.log(nomess[1]);
+
+//Crie uma lista com nomes e mostre apenas o terceiro elemento.
+let nomesss = ['Denis','Bianca','Fawkes'];
+console.log(nomesss.pop());
